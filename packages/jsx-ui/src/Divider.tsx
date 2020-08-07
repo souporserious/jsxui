c46f41471314302dbcc2a976ebc547ed6b91c383
Travis Arnold <ftntravis@gmail.com>
2020-08-06 17:13:42 -0700

    fix Divider collapsing

--- packages/jsx-ui/src/Divider.tsx
+++ packages/jsx-ui/src/Divider.tsx
@@ -12,18 +12,18 @@ export type DividerProps = {
 export const Divider = React.forwardRef<HTMLDivElement, DividerProps>(
   (props, ref) => {
     const parentAxis = React.useContext(StackContext)
-    const { color = '#000', column, row, size = 1 } = useModifierProps<
-      DividerProps
-    >(Divider, props)
+    const { color = '#000', size = 1 } = useModifierProps<DividerProps>(
+      Divider,
+      props
+    )
     const isHorizontal = parentAxis === 'horizontal'
     return (
       <div
         ref={ref}
         style={{
-          gridColumn: column,
-          gridRow: row,
           width: isHorizontal ? size : '100%',
           height: isHorizontal ? '100%' : size,
+          flexShrink: 0,
           backgroundColor: color,
         }}
       />
