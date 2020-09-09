d28f3f4ba3cb3c563e0f7647644e6bbddf2a320d
Travis Arnold <ftntravis@gmail.com>
2020-09-08 21:54:14 -0700

    add variants to Tokens component

--- packages/jsx-ui/src/Tokens.tsx
+++ packages/jsx-ui/src/Tokens.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react'
 
+import { useVariantProps } from './Variants'
+
 const TokensContext = React.createContext({})
+
+export type TokenProps = {
+  value?: any
+  variants?: object
+  children?: React.ReactNode
+}
 
 export function useTokens(): { [key in any]: any } {
   return React.useContext(TokensContext)
 }
 
-export function Tokens({ value, children }) {
+export function Tokens({ children, value, variants }) {
   const tokens = React.useContext(TokensContext)
+  const variantProps = useVariantProps({ variants, ...value })
   return (
-    <TokensContext.Provider value={{ ...value, ...tokens }}>
+    <TokensContext.Provider value={{ ...tokens, ...variantProps }}>
       {children}
     </TokensContext.Provider>
   )
