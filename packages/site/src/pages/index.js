e1cb9068528767271f2b8a0d672b33a0d3e17e01
Travis Arnold <ftntravis@gmail.com>
2020-12-19 17:50:28 -0800

    remove unused code

--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -1,14 +1,5 @@
 import React from 'react'
-import {
-  Circle,
-  Text,
-  Tokens,
-  Spacer,
-  Stack,
-  Overrides,
-  Graphic,
-} from '@jsxui/react'
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import { Text, Tokens, Spacer, Stack } from '@jsxui/react'
 
 import { Github, Logo, Shapes, Wave } from '../assets'
 
@@ -146,63 +137,3 @@ export default function Index() {
     </Tokens>
   )
 }
-
-// <Overrides value={[<Text color="white" />]}>
-// {/* Can we make using fractional sizes along both axes easier? Auto wrap children/use Grid? */}
-// <Stack axis="x" spaceX="minmax(16px, 1fr)" background="#7B5AD9">
-//   <Stack axis="x" width="minmax(200px, 960px)" spaceBetween="32px">
-//     <Stack
-//       width="1fr"
-//       //spaceXStart="16px" // since we wrap children when using space props this is causing issues
-//       spaceBetween="32px"
-//     >
-//       <Text size="large">Overrides</Text>
-//       <Text lineSpacing={16}>
-//         Overrides are cascading props that allow changing a
-//         component's default props in a tree of components and are
-//         merged in before instance props. It is advised to use
-//         overrides rather than `defaultProps` or default arguments when
-//         you want to allow a user to control the default props of your
-//         component.
-//       </Text>
-//     </Stack>
-//     <Stack width="1fr" background="#6d47d7">
-//       <LiveProvider code="<strong>Hello World!</strong>">
-//         <LiveEditor />
-//         <LiveError />
-//         {/* <LivePreview /> */}
-//       </LiveProvider>
-//     </Stack>
-//   </Stack>
-// </Stack>
-// </Overrides>
-// <Spacer size="minmax(80px, 1fr)" background="#7B5AD9" />
-// <Overrides value={[<Text color="white" />]}>
-// <Stack axis="x" spaceX="minmax(16px, 1fr)" background="#7B5AD9">
-//   <Stack axis="x" width="minmax(200px, 960px)" spaceBetween="32px">
-//     <Stack
-//       width="1fr"
-//       //spaceXStart="16px"
-//       spaceBetween="32px"
-//     >
-//       <Text size="large">Variants</Text>
-//       <Text lineSpacing={16}>
-//         Variants allow aliasing a set of props that can be activated
-//         and deactivated. These are helpful for things like:
-//         <ul>
-//           <li>Media Queries</li>
-//           <li>A/B Testing</li>
-//           <li>User Roles</li>
-//         </ul>
-//       </Text>
-//     </Stack>
-//     <Stack width="1fr" background="#6d47d7">
-//       <LiveProvider code="<strong>Hello World!</strong>">
-//         <LiveEditor />
-//         <LiveError />
-//         {/* <LivePreview /> */}
-//       </LiveProvider>
-//     </Stack>
-//   </Stack>
-// </Stack>
-// </Overrides>
