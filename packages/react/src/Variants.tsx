775687e4ebcabd441159950c7d660a6fc8c8d432
Travis Arnold <ftntravis@gmail.com>
2021-01-29 00:34:18 -0800

    feat(Variants): pass state to functions (#18)

--- packages/react/src/Variants.tsx
+++ packages/react/src/Variants.tsx
@@ -5,28 +5,26 @@ export const VariantsContext = React.createContext([])
 export function getVariantProps(variantsContext, { variants, ...props }: any) {
   let mergedProps = { ...props }
   if (variants) {
-    Object.entries(variantsContext).forEach(([variant, active]) => {
+    Object.entries(variantsContext).forEach(([variant, state]) => {
       const variantProps = variants[variant]
-      // we intentionally call each function no matter what since variants can
-      // contain hooks and need to run in the same order on every render
-      const nextProps =
-        typeof variantProps === 'function'
-          ? variantProps(mergedProps, active)
-          : variantProps
-      if (active) {
-        mergedProps = {
-          ...mergedProps,
-          ...nextProps,
+      if (state) {
+        if (typeof variantProps === 'function') {
+          mergedProps = variantProps(mergedProps, state)
+        } else {
+          mergedProps = {
+            ...mergedProps,
+            ...variantProps,
+          }
         }
       }
     })
   }
   // check and convert boolean props that have a variant value
   // <Text visible="checkout-success">Success!</Text>
-  Object.entries(variantsContext).forEach(([variant, active]) => {
+  Object.entries(variantsContext).forEach(([variant, state]) => {
     Object.entries(props).forEach(([prop, value]) => {
       if (value === variant) {
-        mergedProps[prop] = active
+        mergedProps[prop] = state
       }
     })
   })
